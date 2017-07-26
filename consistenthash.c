6eb004a6dd14b6108814622d0b5f2a5706c0ecfb
drdr xp <drdr.xp@gmail.com>
2017-07-26 11:48:40 +0800

    compile with luajit

--- consistenthash.c
+++ consistenthash.c
@@ -7,6 +7,7 @@
 #include <lua.h>
 #include <lauxlib.h>
 
+#include "md5.h"
 
 #define LIB_VERSION              "lua-consistent-hash-module 0.1"
 #define LUA_CONSIS_HASH_TYPENAME "lua_consis_hash"
@@ -79,280 +80,6 @@ typedef struct {
 } continuum_t;
 
 
-typedef struct {
-    uint64_t  bytes;
-    uint32_t  a, b, c, d;
-    u_char    buffer[64];
-} md5_t;
-
-
-void md5_init(md5_t *ctx);
-void md5_update(md5_t *ctx, const void *data, size_t size);
-void md5_final(u_char result[16], md5_t *ctx);
-
-
-static const u_char *md5_body(md5_t *ctx, const u_char *data, size_t size);
-
-
-void
-md5_init(md5_t *ctx)	//初始链接变量
-{
-    ctx->a = 0x67452301;
-    ctx->b = 0xefcdab89;
-    ctx->c = 0x98badcfe;
-    ctx->d = 0x10325476;
-
-    ctx->bytes = 0;
-}
-
-
-void
-md5_update(md5_t *ctx, const void *data, size_t size)	//将字节串分组处理
-{
-    size_t  used, free;
-
-    used = (size_t) (ctx->bytes & 0x3f);
-    ctx->bytes += size;
-
-    if (used) {
-        free = 64 - used;
-
-        if (size < free) {
-            memcpy(&ctx->buffer[used], data, size);
-            return;
-        }
-
-        memcpy(&ctx->buffer[used], data, free);
-        data = (u_char *) data + free;
-        size -= free;
-        (void) md5_body(ctx, ctx->buffer, 64);
-    }
-
-    if (size >= 64) {
-        data = md5_body(ctx, data, size & ~(size_t) 0x3f);
-        size &= 0x3f;
-    }
-
-    memcpy(ctx->buffer, data, size);
-}
-
-
-void
-md5_final(u_char result[16], md5_t *ctx)	//byte转unsigned int
-{
-    size_t  used, free;
-
-    used = (size_t) (ctx->bytes & 0x3f);
-
-    ctx->buffer[used++] = 0x80;
-
-    free = 64 - used;
-
-    if (free < 8) {
-        memzero(&ctx->buffer[used], free);
-        (void) md5_body(ctx, ctx->buffer, 64);
-        used = 0;
-        free = 64;
-    }
-
-    memzero(&ctx->buffer[used], free - 8);
-
-    ctx->bytes <<= 3;
-    ctx->buffer[56] = (u_char) ctx->bytes;
-    ctx->buffer[57] = (u_char) (ctx->bytes >> 8);
-    ctx->buffer[58] = (u_char) (ctx->bytes >> 16);
-    ctx->buffer[59] = (u_char) (ctx->bytes >> 24);
-    ctx->buffer[60] = (u_char) (ctx->bytes >> 32);
-    ctx->buffer[61] = (u_char) (ctx->bytes >> 40);
-    ctx->buffer[62] = (u_char) (ctx->bytes >> 48);
-    ctx->buffer[63] = (u_char) (ctx->bytes >> 56);
-
-    (void) md5_body(ctx, ctx->buffer, 64);
-
-    result[0] = (u_char) ctx->a;
-    result[1] = (u_char) (ctx->a >> 8);
-    result[2] = (u_char) (ctx->a >> 16);
-    result[3] = (u_char) (ctx->a >> 24);
-    result[4] = (u_char) ctx->b;
-    result[5] = (u_char) (ctx->b >> 8);
-    result[6] = (u_char) (ctx->b >> 16);
-    result[7] = (u_char) (ctx->b >> 24);
-    result[8] = (u_char) ctx->c;
-    result[9] = (u_char) (ctx->c >> 8);
-    result[10] = (u_char) (ctx->c >> 16);
-    result[11] = (u_char) (ctx->c >> 24);
-    result[12] = (u_char) ctx->d;
-    result[13] = (u_char) (ctx->d >> 8);
-    result[14] = (u_char) (ctx->d >> 16);
-    result[15] = (u_char) (ctx->d >> 24);
-
-    memzero(ctx, sizeof(*ctx));
-}
-
-
-/*
- * The basic MD5 functions.
- *
- * F and G are optimized compared to their RFC 1321 definitions for
- * architectures that lack an AND-NOT instruction, just like in
- * Colin Plumb's implementation.
- */
-
-#define F(x, y, z)  ((z) ^ ((x) & ((y) ^ (z))))
-#define G(x, y, z)  ((y) ^ ((z) & ((x) ^ (y))))
-#define H(x, y, z)  ((x) ^ (y) ^ (z))
-#define I(x, y, z)  ((y) ^ ((x) | ~(z)))
-
-
-/*
- * The MD5 transformation for all four rounds.
- */
-
-#define STEP(f, a, b, c, d, x, t, s)                                          \
-    (a) += f((b), (c), (d)) + (x) + (t);                                      \
-    (a) = (((a) << (s)) | (((a) & 0xffffffff) >> (32 - (s))));                \
-    (a) += (b)
-
-
-/*
- * SET() reads 4 input bytes in little-endian byte order and stores them
- * in a properly aligned word in host byte order.
- *
- * The check for little-endian architectures that tolerate unaligned
- * memory accesses is just an optimization.  Nothing will break if it
- * does not work.
- */
-
-#define SET(n)                                                                \
-    (block[n] =                                                               \
-    (uint32_t) p[n * 4] |                                                     \
-    ((uint32_t) p[n * 4 + 1] << 8) |                                          \
-    ((uint32_t) p[n * 4 + 2] << 16) |                                         \
-    ((uint32_t) p[n * 4 + 3] << 24))
-
-#define GET(n)      block[n]
-
-
-/*
- * This processes one or more 64-byte data blocks, but does not update
- * the bit counters.  There are no alignment requirements.
- */
-
-static const u_char *
-md5_body(md5_t *ctx, const u_char *data, size_t size)	//四轮操作
-{
-    uint32_t       a, b, c, d;
-    uint32_t       saved_a, saved_b, saved_c, saved_d;
-    const u_char  *p;
-    uint32_t       block[16];
-
-    p = data;
-
-    a = ctx->a;
-    b = ctx->b;
-    c = ctx->c;
-    d = ctx->d;
-
-    do {
-        saved_a = a;
-        saved_b = b;
-        saved_c = c;
-        saved_d = d;
-
-        /* Round 1 */
-
-        STEP(F, a, b, c, d, SET(0),  0xd76aa478, 7);
-        STEP(F, d, a, b, c, SET(1),  0xe8c7b756, 12);
-        STEP(F, c, d, a, b, SET(2),  0x242070db, 17);
-        STEP(F, b, c, d, a, SET(3),  0xc1bdceee, 22);
-        STEP(F, a, b, c, d, SET(4),  0xf57c0faf, 7);
-        STEP(F, d, a, b, c, SET(5),  0x4787c62a, 12);
-        STEP(F, c, d, a, b, SET(6),  0xa8304613, 17);
-        STEP(F, b, c, d, a, SET(7),  0xfd469501, 22);
-        STEP(F, a, b, c, d, SET(8),  0x698098d8, 7);
-        STEP(F, d, a, b, c, SET(9),  0x8b44f7af, 12);
-        STEP(F, c, d, a, b, SET(10), 0xffff5bb1, 17);
-        STEP(F, b, c, d, a, SET(11), 0x895cd7be, 22);
-        STEP(F, a, b, c, d, SET(12), 0x6b901122, 7);
-        STEP(F, d, a, b, c, SET(13), 0xfd987193, 12);
-        STEP(F, c, d, a, b, SET(14), 0xa679438e, 17);
-        STEP(F, b, c, d, a, SET(15), 0x49b40821, 22);
-
-        /* Round 2 */
-
-        STEP(G, a, b, c, d, GET(1),  0xf61e2562, 5);
-        STEP(G, d, a, b, c, GET(6),  0xc040b340, 9);
-        STEP(G, c, d, a, b, GET(11), 0x265e5a51, 14);
-        STEP(G, b, c, d, a, GET(0),  0xe9b6c7aa, 20);
-        STEP(G, a, b, c, d, GET(5),  0xd62f105d, 5);
-        STEP(G, d, a, b, c, GET(10), 0x02441453, 9);
-        STEP(G, c, d, a, b, GET(15), 0xd8a1e681, 14);
-        STEP(G, b, c, d, a, GET(4),  0xe7d3fbc8, 20);
-        STEP(G, a, b, c, d, GET(9),  0x21e1cde6, 5);
-        STEP(G, d, a, b, c, GET(14), 0xc33707d6, 9);
-        STEP(G, c, d, a, b, GET(3),  0xf4d50d87, 14);
-        STEP(G, b, c, d, a, GET(8),  0x455a14ed, 20);
-        STEP(G, a, b, c, d, GET(13), 0xa9e3e905, 5);
-        STEP(G, d, a, b, c, GET(2),  0xfcefa3f8, 9);
-        STEP(G, c, d, a, b, GET(7),  0x676f02d9, 14);
-        STEP(G, b, c, d, a, GET(12), 0x8d2a4c8a, 20);
-
-        /* Round 3 */
-
-        STEP(H, a, b, c, d, GET(5),  0xfffa3942, 4);
-        STEP(H, d, a, b, c, GET(8),  0x8771f681, 11);
-        STEP(H, c, d, a, b, GET(11), 0x6d9d6122, 16);
-        STEP(H, b, c, d, a, GET(14), 0xfde5380c, 23);
-        STEP(H, a, b, c, d, GET(1),  0xa4beea44, 4);
-        STEP(H, d, a, b, c, GET(4),  0x4bdecfa9, 11);
-        STEP(H, c, d, a, b, GET(7),  0xf6bb4b60, 16);
-        STEP(H, b, c, d, a, GET(10), 0xbebfbc70, 23);
-        STEP(H, a, b, c, d, GET(13), 0x289b7ec6, 4);
-        STEP(H, d, a, b, c, GET(0),  0xeaa127fa, 11);
-        STEP(H, c, d, a, b, GET(3),  0xd4ef3085, 16);
-        STEP(H, b, c, d, a, GET(6),  0x04881d05, 23);
-        STEP(H, a, b, c, d, GET(9),  0xd9d4d039, 4);
-        STEP(H, d, a, b, c, GET(12), 0xe6db99e5, 11);
-        STEP(H, c, d, a, b, GET(15), 0x1fa27cf8, 16);
-        STEP(H, b, c, d, a, GET(2),  0xc4ac5665, 23);
-
-        /* Round 4 */
-
-        STEP(I, a, b, c, d, GET(0),  0xf4292244, 6);
-        STEP(I, d, a, b, c, GET(7),  0x432aff97, 10);
-        STEP(I, c, d, a, b, GET(14), 0xab9423a7, 15);
-        STEP(I, b, c, d, a, GET(5),  0xfc93a039, 21);
-        STEP(I, a, b, c, d, GET(12), 0x655b59c3, 6);
-        STEP(I, d, a, b, c, GET(3),  0x8f0ccc92, 10);
-        STEP(I, c, d, a, b, GET(10), 0xffeff47d, 15);
-        STEP(I, b, c, d, a, GET(1),  0x85845dd1, 21);
-        STEP(I, a, b, c, d, GET(8),  0x6fa87e4f, 6);
-        STEP(I, d, a, b, c, GET(15), 0xfe2ce6e0, 10);
-        STEP(I, c, d, a, b, GET(6),  0xa3014314, 15);
-        STEP(I, b, c, d, a, GET(13), 0x4e0811a1, 21);
-        STEP(I, a, b, c, d, GET(4),  0xf7537e82, 6);
-        STEP(I, d, a, b, c, GET(11), 0xbd3af235, 10);
-        STEP(I, c, d, a, b, GET(2),  0x2ad7d2bb, 15);
-        STEP(I, b, c, d, a, GET(9),  0xeb86d391, 21);
-
-        a += saved_a;
-        b += saved_b;
-        c += saved_c;
-        d += saved_d;
-
-        p += 64;
-
-    } while (size -= 64);
-
-    ctx->a = a;
-    ctx->b = b;
-    ctx->c = c;
-    ctx->d = d;
-
-    return p;
-}
-
-
 static uint32_t  crc32_table16[] = {
     0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
     0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
@@ -364,7 +91,7 @@ uint32_t *crc32_table_short = crc32_table16;
 
 
 static inline uint32_t
-crc32_short(u_char *p, size_t len)	//crc编码
+crc32_short(u_char *p, size_t len)
 {
     u_char    c;
     uint32_t  crc;
@@ -382,8 +109,10 @@ crc32_short(u_char *p, size_t len)	//crc编码
 
 
 static int
-node_cmp( const hashnode_t *a, const hashnode_t *b )	//比较节点的值，a<b返回-1，a>b返回1，相等返回0
+node_cmp( const hashnode_t *a, const hashnode_t *b )
 {
+    /* for qsort below, sort from small to large */
+
     if ( a->point < b->point ) {
         return -1;
     }
@@ -411,8 +140,8 @@ point_search( continuum_t *conti, uint32_t p )
     }
 
 
-    while ( s + 1 < e ) {	//二分查找
-        mid = ( s+e ) / 2;
+    while ( s + 1 < e ) {
+        mid = ( s+e ) / 2 ;
         if ( p <= conti->nodes[ mid ].point ) {
             e = mid;
         }
@@ -447,13 +176,13 @@ static int
 lch_get( lua_State *L )
 {
     void        **p;
-    continuum_t  *conti;	//一个continuum结构指针
+    continuum_t  *conti;
 
     size_t        len;
-    const char   *s;	//一个给定的内容
-    uint32_t      hash_value;	//这个内容经编码得到的hash值
-    size_t        i_node;	//用于节点计数？？？
-    int           n_replica;	//节点的虚拟节点个数？？？
+    const char   *s;
+    uint32_t      hash_value;
+    size_t        i_node;
+    int           n_replica;
     int           i;
     str_t        *name;
     int           i_name;
@@ -465,33 +194,33 @@ lch_get( lua_State *L )
     p = get_continuum( L, 1 );
     conti = *p;
 
-    s = luaL_checklstring( L, 2, &len );	//判断是否为字符串，是则返回长度，否则填充1
+    s = luaL_checklstring( L, 2, &len );
 
-    if ( lua_isnumber( L, 3 ) ) {	//？？？
-        n_replica = lua_tointeger( L, 3 );	//得到这个节点的虚拟节点个数
-		if ( n_replica > MAX_REPLICA ) {	//虚拟节点最多为16个
+    if ( lua_isnumber( L, 3 ) ) {
+        n_replica = lua_tointeger( L, 3 );
+        if ( n_replica > MAX_REPLICA ) {
             n_replica = MAX_REPLICA;
         }
-        if ( n_replica >= conti->n ) {	//虚拟节点个数不超过已有空间的大小（不知道continuum的翻译成什么比较好……）
+        if ( n_replica >= conti->n ) {
             n_replica = conti->n;
         }
     }
     else {
-        n_replica = 1;	//？？？
+        n_replica = 1;
     }
 
-    derr( "n_replica=%d", n_replica );	//打印n_replica
+    derr( "n_replica=%d", n_replica );
 
-    hash_value = ( uint32_t )crc32_short( ( u_char* )s, len );	//把s进行crc编码
-    hash_value = hash_value >> 18;	//右移18位
-    derr( "hash value of %.*s is %d", len, s, hash_value );		//打印哈希值
+    hash_value = ( uint32_t )crc32_short( ( u_char* )s, len );
+    hash_value = hash_value >> 18;
+    derr( "hash value of %.*s is %d", len, s, hash_value );
 
-    i_node = conti->buckets[ hash_value ];	//？？？在continuum里按照刚得到的哈希值找到的位置赋值给i_node
-
+    /*find the bucket fot the node according to its hash value*/
+    i_node = conti->buckets[ hash_value ];
     for ( i = 0; i < MAX_REPLICA; i++ ) {
-        i_names[i] = -1;	//初始化所有虚拟节点名称为-1
+        i_names[i] = -1;
     }
-    n_names = 0;	//？？？当前用的虚拟节点数为0
+    n_names = 0;
 
     while ( n_names < n_replica ) {
 
@@ -552,7 +281,7 @@ lch_new( lua_State *L ) {
 
     /* store md5 result and hash_value at the same place. */
     uint32_t      hash_value[16/sizeof(uint32_t)];
-    u_char       *md5_rst = (u_char*)hash_value;	//加入新数据时，先得到一个hash值
+    u_char       *md5_rst = (u_char*)hash_value;
 
 
     luaL_checktype( L, 2, LUA_TTABLE );
@@ -562,9 +291,9 @@ lch_new( lua_State *L ) {
     n = 0;
     n_nodes = 0;
     namebuf_size = 0;
-    lua_pushnil( L ); /* iterate all table entries from nil key */
+    lua_pushnil( L );
 
-    while ( lua_next( L, -2 ) ) {	//遍历，把索引中的key值压入堆栈
+    while ( lua_next( L, -2 ) ) {
 
         s = lua_tolstring( L, -1, &len );
         if ( NULL == s ) {
@@ -579,20 +308,20 @@ lch_new( lua_State *L ) {
             return 0;
         }
 
-        lua_pop( L, 1 ); /* pop up value, leave key in stack */
-        n++;	//每遍历一个key值，n计数加一
+        lua_pop( L, 1 );
+        n++;
         namebuf_size += len;
         n_nodes += N_NODE_PER_NAME * 1; /* TODO weights */
 
         dd( "entry: %s len=%d, total namebuf_size=%d", s, len, namebuf_size );
     }
 
-    if ( 0 == n ) {		//如果table为空，输出错误信息
-		luaL_argerror( L, 2, "Empty table" );
+    if ( 0 == n ) {
+        luaL_argerror( L, 2, "Empty table" );
         return 0;
     }
 
-
+    /*caculate the total memory needed by the continuum*/
     size = sizeof( continuum_t )
             + sizeof( str_t ) * n
             + namebuf_size
@@ -602,14 +331,14 @@ lch_new( lua_State *L ) {
 
     dd( "total mem size=%d", size );
 
-    buf = malloc( size );	//分配内存
+    buf = malloc( size );
     if ( NULL == buf ) {
         derr( "Failure allocation memory of size=%d", size );
         return 0;
     }
 
     dinfo( "Allocated continuum mem: %p", buf );
-	//为continuum_t中每一成员分配内存
+
     conti = ( continuum_t* )buf;
     buf += sizeof( continuum_t );
 
@@ -635,7 +364,7 @@ lch_new( lua_State *L ) {
     namebuf_p = 0;
     lua_pushnil( L );
 
-    while ( lua_next( L, -2 ) ) {	//？？？遍历打印信息
+    while ( lua_next( L, -2 ) ) {
 
         s = lua_tolstring( L, -1, &len );
 
@@ -646,7 +375,7 @@ lch_new( lua_State *L ) {
 
         dd( "built name: for %d-th name: len=%d name=%.*s", i, len, len, s );
 
-        lua_pop( L, 1 ); /* pop up value, leave key in stack */
+        lua_pop( L, 1 );
         i++;
         namebuf_p += len;
     }
@@ -684,6 +413,9 @@ lch_new( lua_State *L ) {
 
     step = (uint32_t) (0xffffffff / conti->n_buckets);
     for ( i = 0; i < conti->n_buckets; i++ ) {
+
+        /*map the node to a bucket*/
+
         i_nodes = point_search( conti, step * i );
         conti->buckets[ i ] = i_nodes;
         k = conti->nodes[ i_nodes ].name_idx;
@@ -806,9 +538,9 @@ main( int argc, char **argv )
         exit( 1 );
     }
 
-    lua_getglobal( L, "main" );	//把main压入栈
+    lua_getglobal( L, "main" );
 
-    rc = lua_pcall( L, 0, 3, 0 );	//以保护模式调用函数，有错误会捕获并把信息压入栈
+    rc = lua_pcall( L, 0, 3, 0 );
     if ( 0 != rc )
     {
         err = lua_tostring( L, -1 );
